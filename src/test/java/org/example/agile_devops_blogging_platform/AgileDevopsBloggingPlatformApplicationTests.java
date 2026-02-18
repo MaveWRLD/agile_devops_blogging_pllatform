f6c2427b56a917eb31c703fdc7c2bdad4f763d05
Jacob Quaye <kofimave@gmail.com>
2026-02-18 14:00:04 +0000

    feat: Configure H2 in-memory DB for SpringBoot tests in CI

--- src/test/java/org/example/agile_devops_blogging_platform/AgileDevopsBloggingPlatformApplicationTests.java
+++ src/test/java/org/example/agile_devops_blogging_platform/AgileDevopsBloggingPlatformApplicationTests.java
@@ -2,12 +2,15 @@ package org.example.agile_devops_blogging_platform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AgileDevopsBloggingPlatformApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
+
