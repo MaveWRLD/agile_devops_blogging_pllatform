755403e7f9089276106cd193e9376775d22e87d3
Jacob Quaye <kofimave@gmail.com>
2026-02-18 09:25:16 +0000

    refactor: correct file name from AgileDevopsBloggingPlatformAppllication to AgileDevopsBloggingPlatformApplication

--- src/test/java/org/example/agile_devops_blogging_platform/AgileDevopsBloggingPlatformApplicationTests.java
+++ src/test/java/org/example/agile_devops_blogging_platform/AgileDevopsBloggingPlatformApplicationTests.java
@@ -1,10 +1,10 @@
-package org.example.agile_devops_blogging_pllatform;
+package org.example.agile_devops_blogging_platform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AgileDevopsBloggingPllatformApplicationTests {
+class AgileDevopsBloggingPlatformApplicationTests {
 
     @Test
     void contextLoads() {
