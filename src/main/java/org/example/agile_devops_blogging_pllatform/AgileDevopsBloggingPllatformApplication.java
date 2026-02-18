a60bd83d8e024921cec42fe62302a1c6c9e12946
Jacob Quaye <kofimave@gmail.com>
2026-02-18 09:14:45 +0000

    feat: initial project setup

---
+++ src/main/java/org/example/agile_devops_blogging_pllatform/AgileDevopsBloggingPllatformApplication.java
@@ -0,0 +1,13 @@
+package org.example.agile_devops_blogging_pllatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileDevopsBloggingPllatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgileDevopsBloggingPllatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/agile_devops_blogging_pllatform/AgileDevopsBloggingPllatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.agile_devops_blogging_pllatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileDevopsBloggingPllatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
